3a1a5c223a7f8387a9fbcde730ad9469122f6586
Awono-237 <awonofoelazare@gmail.com>
2026-01-22 20:56:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// ===== MODE SOMBRE / CLAIR =====
+const themeBtn = document.getElementById("themeToggle");
+// Charger le thème sauvegardé
+if (localStorage.getItem("theme") === "dark") {
+document.body.classList.add("dark");
+themeBtn.textContent = "☀";
+}
+// Changer le thème au clic
+themeBtn.addEventListener("click", () => {
+document.body.classList.toggle("dark");
+if (document.body.classList.contains("dark")) {
+themeBtn.textContent = "☀";
+localStorage.setItem("theme", "dark");
+} else {
+themeBtn.textContent = "🌙";
+localStorage.setItem("theme", "light");
+}
+});
+// ===== ANIMATION AU SCROLL =====
+const sections = document.querySelectorAll("section");
+function revealOnScroll() {
+sections.forEach(section => {
+const position = section.getBoundingClientRect().top;
+const screenHeight = window.innerHeight;
+if (position < screenHeight - 100) {
+section.classList.add("show");
+}
+});
+}
+window.addEventListener("scroll", revealOnScroll)
